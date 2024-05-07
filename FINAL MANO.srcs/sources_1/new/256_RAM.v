2d8f8339a695fdc2db4142e4ed2d9d8de45ad58d
Ali Khamis <134519799+alikhamisabdelmotelb@users.noreply.github.com>
2024-05-08 00:11:43 +0300

    Update 256_RAM.v

--- FINAL MANO.srcs/sources_1/new/256_RAM.v
+++ FINAL MANO.srcs/sources_1/new/256_RAM.v
@@ -5,23 +5,23 @@ module RAM (CLK,read,AR,write,INDATA,OUTDATA );
     input [11:0] AR;
     output reg [15:0] OUTDATA;
     
-    reg [15:0] RAM_4K [4095:0];
+    reg [15:0] RAM [255:0];
         
     initial 
     begin
-    RAM_4K[0] = 16'h7020;       //INR AC
-    RAM_4K[1] = 16'h7800;       //clear AC
-    RAM_4K[2] = 16'h7200;       //CMA
-    RAM_4K[3] = 16'h2004;       //LDA      
-    RAM_4K[4] = 16'h2002;       //ADD
-    RAM_4K[5] = 16'h7001;       //HLT
+    RAM[0] = 16'h7020;       //INR AC
+    RAM[1] = 16'h7800;       //clear AC
+    RAM[2] = 16'h7200;       //CMA
+    RAM[3] = 16'h2004;       //LDA      
+    RAM[4] = 16'h2002;       //ADD
+    RAM[5] = 16'h7001;       //HLT
     end
     
     always@* begin
         if (write)
-            RAM_4K[AR] <= INDATA;
+            RAM[AR] <= INDATA;
         else if(read)
-            OUTDATA = RAM_4K[AR];
+            OUTDATA = RAM[AR];
         end
 
 endmodule
