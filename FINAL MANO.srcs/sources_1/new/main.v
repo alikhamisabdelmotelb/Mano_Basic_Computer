97d90c5ac311a244fa647a67ec0dbca0ad18b69f
Ali Khamis <134519799+alikhamisabdelmotelb@users.noreply.github.com>
2024-05-08 05:33:07 +0300

    Update main.v

--- FINAL MANO.srcs/sources_1/new/main.v
+++ FINAL MANO.srcs/sources_1/new/main.v
@@ -1,9 +1,6 @@
 
  module Mainn(input CLK,
-     /*
-     , output E,
-     output reg[15:0] AC,DR
-     */
+
      output wire [15:0] DR, AC, IR, MEM,
      output wire [11:0] PC, AR,
      output wire [7:0] T ,D,
@@ -13,7 +10,7 @@
      output wire [7:0] X ,
      output wire I,
      output wire E
-     );//????????????????????
+     );
      
 	wire   INRAR, LoadAR, CLRAR,
 	       INRPC, LoadPC, CLRPC,
@@ -26,7 +23,7 @@
 	//////////////////////////
 	wire INRSC;
 	/////////////////////////
-	wire  cout; //?
+	wire  cout; 
 	assign E =cout ; 
 	/////////////////////////
 	wire [15:0] m1, m2, m3, m4, m5 ,m7;         // (reg) input wires of the bus selection 
@@ -56,12 +53,12 @@
 	
 	
 	// IR & IR control -------------------------------
-	//reg[15:0]IR ;	                             ////IR ?????output ?????????????
+	//reg[15:0]IR ;	                             
 	REG_16 ir_reg(IR, INRIR, Data, LoadIR, CLK, CLRIR);                // DR_reg(LD,CLK,INR,CLR,inDR,DR );
     
     //assign IR = LoadIR ? Data :IR; // IR  IS THE OUTPUT OF THE IR REG                     
 	   
-	assign m5 = IR;                              /////???????????????? I THINK m5= IR 
+	assign m5 = IR;                        
 	
     // CONTROL UNIT ------------------------------------
     controlUnit CTRLunit( CLK, T, cout, AC, IR,
@@ -77,7 +74,7 @@
                           D);  //instatiating   control unit 
 
     //BUS SYSTEM --------------------------------------- 
-	Encoder_8x3 SEL_ENC(SSS,X);	                                        //instatiating bus control & connecting x (as an input)from encoder to x (as an output to)control unit
+	Encoder_8x3 SEL_ENC(SSS,X);	                                  
 	MUX BUS_SELECTION (16'bxxxxxxxxxxxxxxxx, m1, m2, m3, m4, m5, 16'b0000000000000000, m7, SSS, Data);  // MUX(d0,d1,d2,d3,d4,d5,d6,d7,sel,out);
 	
 	// Sequence COUNTER ---------------------------------------------------------
@@ -85,8 +82,5 @@
 	assign INRSC = ~CLRSC;                             // CLRsc is an output from control unit (control sc)
 	SequenceCounter sc (CLRSC, INRSC , CLK, outsc);    //  module SequenceCounter(CLR, INC, CLK, out); 
 	Decoder3to8 dec(outsc, T);                   
-	 
-				
-
                                                
 endmodule
